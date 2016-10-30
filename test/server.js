a72ef01a9f31891f9aab390beac74acdd04aa32d
0x8890 <0x8890@airmail.cc>
2016-10-30 22:05:33 +0100

    update dependencies

--- test/server.js
+++ test/server.js
@@ -8,6 +8,6 @@ const port = 8890
 testInstance()
 .then(instance => fortuneWS(instance, (state, changes) => {
   if (changes) return changes
-  if (state.kill) setTimeout(() => process.exit(), 500)
+  if (state.kill) setTimeout(() => process.exit(), 100)
   return state
 }, { port }))
